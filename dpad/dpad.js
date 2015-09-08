b8269514177f7f0e689e9c29930d0bb10ee5e341
Francois <francois@n-dream.com>
2015-09-08 10:49:00 +0200

    Improved code. Readme adapted

--- dpad/dpad.js
+++ dpad/dpad.js
@@ -8,8 +8,10 @@
  * @property {DPad~touchEndCallback} touchend -
  *           The callback that gets called when the DPad is released
  * @property {number|DPad~Coordinate|undefined} distance - amount of pixels
- *           which the user needs to move the DPad before triggering a
+ *           which the user needs to move or tap the DPad before triggering a
  *           direction. E.g: { x: 10, y: 10}
+ * @property {boolean|undefined} relative - If true, dpad works by relative swipe.
+             Default: false
  * @property {boolean|undefined} diagonal - If true, diagonal movement are
  *           possible and it becomes a 8-way DPad: For exmaple UP and RIGHT at
  *           the same time. Default: false
@@ -49,14 +51,13 @@
 function DPad(el, opts) {
   var me = this;
   opts = opts || {}
-  me.is_relative = opts.distance || false;
+  me.is_relative = opts.relative || false;
   opts.distance = opts.distance || { x: 10, y: 10};
   me.distance = {
     x: opts.distance.x || opts.distance,
     y: opts.distance.y || opts.distance
   };
   me.diagonal = opts.diagonal || false;
-  me.possible_directions = [];
 
   var log_cb = function(name) {
     return function (key, pressed) {
@@ -87,7 +88,6 @@ function DPad(el, opts) {
   }
 
   this.bindEvents();
-  this.generateDirections();
 
   me.state = {};
   me.state[DPad.UP] = false;
@@ -347,40 +347,11 @@ DPad.prototype.getEventPoint = function(e) {
 };
 
 /**
- * Set one or multiple directions
- * @param {Array} directins - List of DPad~Coordinate values
- */
-DPad.prototype.setDirections = function(directions) {
-  for (var i = 0; i < directions.length; i++) {
-    var state = this.state[directions[i]];
-    if (state !== true) {
-      this.setState(directions[i], true);
-    }
-  }
-};
-
-/**
  * Gets called when the absolute DPad gets touched
  * @param {DPad~Coordinate} pos - The position of the initial touch.
  */
 DPad.prototype.onStartAbsolute = function(pos) {
-  var directions = this.getActiveArea(pos);
-  this.setDirections(directions);
-};
-
-/**
- * Returns currently active directions
- * @return {Array}
- */
-DPad.prototype.getActiveDirections = function() {
-  var dirs = [];
-  for (var dir in this.state) {
-    var state = this.state[dir];
-    if (state === true) {
-      dirs.push(dir);
-    }
-  }
-  return dirs;
+  this.onMoveAbsolute(pos);
 };
 
 /**
@@ -389,118 +360,38 @@ DPad.prototype.getActiveDirections = function() {
  * @param {DPad~Coordinate} pos - The position of the current touch point
  */
 DPad.prototype.onMoveAbsolute = function(pos) {
-  var directions = this.getActiveArea(pos);
-  var active_directions = this.getActiveDirections();
-  var compareArrays = function(arr_a, arr_b) {
-    if (arr_a.length !== arr_b.length) return false;
-    var ca = arr_a.slice().sort().join(",");
-    var cb = arr_b.slice().sort().join(",");
-    return ca === cb;
-  };
-  var all_same = compareArrays(directions, active_directions);
-  if (!all_same) {
-    // Calculate a threshold change distance so that e.g. if you go from left to right
-    // you do not hit bottom or top
-    var container = this.container.getBoundingClientRect();
-    var wh = container.width / 2;
-    var hh = container.height / 2;
-    var center_x = wh;
-    var center_y = hh;
-    var THRESHOLD_PERCENTAGE = 10;
-    var threshold_distance = (THRESHOLD_PERCENTAGE * container.width) / 100;
-    var actual_distance = Math.sqrt(Math.pow(center_x - pos.x, 2) + Math.pow(center_y - pos.y, 2));
-    if (actual_distance > threshold_distance) {
-      this.resetState();
-      this.setDirections(directions);
-    }
-  }
-};
-
-/**
- * Generate and sets possible directions depending on how many
- * buttons the pad has (8 or 4)
- */
-DPad.prototype.generateDirections = function() {
-  var is_diagonal = this.diagonal;
-  var press_directions = [];
-  var directions = [
-    DPad.UP, DPad.RIGHT, DPad.DOWN, DPad.LEFT
-  ];
-  var len = directions.length;
-  var last_index = len - 1;
-  for (var i = 0; i < len; i++) {
-    var prev = i - 1 < 0 ? last_index : i - 1;
-    var next = i + 1 > last_index ? 0 : i + 1;
-    var add_diagonal = i % 2 === 0 && is_diagonal === true;
-    if (add_diagonal) {
-      press_directions.push([directions[i], directions[prev]]);
-    }
-    press_directions.push([directions[i]]);
-    if (add_diagonal) {
-      press_directions.push([directions[i], directions[next]]);
-    }
-  }
-  this.possible_directions = press_directions;
-};
-
-/**
- * Returns the direction-area the point is currently in
- * @param {DPad~Coordinate} pos - The position of the current touch point
- * @return {Array} List of DPad~Position(s)
- */
-DPad.prototype.getActiveArea = function(pos) {
   var container = this.container.getBoundingClientRect();
   var w = container.width;
   var h = container.height;
-  var x = pos.x;
-  var y = pos.y;
-  var angle_steps = [];
-  var result_buttons = [];
-  // Angle a button occupies. E.g 4 buttons = 90
-  var angle_step = 360 / this.possible_directions.length;
-  // Diagonal starts with UP-LEFT, non diagonal with UP direction
-  var start_angle = 180 + angle_step / 2; // E.g. Non-Diagonal: 225
-  // Convert to radian
-  angle_step *= (Math.PI / 180);
-  start_angle *= (Math.PI / 180);
-
-  // Calculate angle steps
-  for (var i = 0; i < this.possible_directions.length; i++) {
-    var step = (start_angle + angle_step * i);
-    if (step >= Math.PI * 2) {
-      step -= Math.PI * 2;
-    }
-    angle_steps.push(step);
-  }
 
   // Calculate reference angle between two vectors in radian
   var vec = {
     x: w / 2,
     y: h / 2
   };
-  var angle = Math.atan2(pos.y - vec.y, pos.x - vec.x);
-  if (angle < 0) angle += 2 * Math.PI;
 
-  var last_angle_index = angle_steps.length - 1;
-  for (var i = 0; i < angle_steps.length; i++) {
-    var ca = angle_steps[i];
-    var next = i + 1 > last_angle_index ? 0 : i + 1;
-    var start = ca;
-    var end = angle_steps[next];
-    // Edge case: Happens for angle which is between the 0 coordinate
-    if (start > end) {
-      if (angle < end) {
-        start = end - angle_step;
-      }
-      if (angle >= start) {
-        end = start + angle_step;
-      }
-    }
-    // Point-angle in angle-sector?
-    if (angle >= start && angle < end) {
-      result_buttons = this.possible_directions[i];
-      break;
-    }
-  };
-   return result_buttons;
+  var angle = Math.atan2(pos.y - vec.y, pos.x - vec.x);
+  if (angle < 0) {
+    angle += 2 * Math.PI;
+  }
+  // NOTE(andrin): This deadzone is the shape of a square instead of an elipse
+  //               for no good reason.
+  if (Math.abs(vec.x - pos.x) > this.distance.x ||
+      Math.abs(vec.y - pos.y) > this.distance.y ) {
+    var diagonal_extension = this.diagonal ? Math.PI / 8 : 0;
+    this.setState(DPad.RIGHT,
+      angle <= Math.PI/4 + diagonal_extension ||
+      angle > Math.PI*7/4-diagonal_extension);
+    this.setState(DPad.DOWN,
+      angle >= Math.PI/4 - diagonal_extension &&
+      angle < Math.PI*3/4 + diagonal_extension);
+    this.setState(DPad.LEFT,
+      angle >= Math.PI*3/4 - diagonal_extension &&
+      angle < Math.PI*5/4 + diagonal_extension);
+    this.setState(DPad.UP,
+      angle >= Math.PI*5/4 - diagonal_extension &&
+      angle < Math.PI*7/4 + diagonal_extension);
+  } else {
+    this.resetState();
+  }
 };
